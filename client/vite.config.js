fa5337bc8967f4626f572acc8745e98b11de5968
akshayvgore <akshayvgore1234@gmail.com>
2025-07-30 12:19:39 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"/My-Job-Sync/",
+  base:"/My-Job-Sync-new/",
   plugins: [react(), tailwindcss()],
 })
